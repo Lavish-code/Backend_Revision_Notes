59f64300c075a438ad44220f59ff506a89c31546
Lavish-code <ylavish07@gmail.com>
2025-10-03 16:50:37 +0530

    added register controller and bugs are fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,82 @@
 import {asyncHandler} from '../utils/asyncHandler.js'
+import {ApiError} from '../utils/ApiError.js'
+import {User} from '../models/user.model.js'
+import {uploadOnCloudinary} from '../utils/cloudinary.js'
+import { ApiResponse } from '../utils/ApiResponse.js'
 
 
 const registerUser = asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message :"Yes Lavish, it's working!!!"
+    //get user details from frontend
+    // check for user validation
+    //check if user already exist : username & email 
+    //check for images , check for avatar
+    //upload them to cloudinary, avatar
+    //create user object - create entry in db 
+    //remove password and refresh token field from response 
+    //check for user creation 
+    //return response
+
+
+    const {fullName, username , email, password} = req.body
+    console.log("email :", email)
+
+    if(
+        [fullName, email, username, password].some((field)=>
+            (field?.trim()===""))
+    ) {
+        throw new ApiError(400,"All fields is required!!")
+    }
+
+    const existedUser = await User.findOne({
+        $or:[{ username },{ email }]
     })
+    if(existedUser){
+        throw new ApiError(409,"User with emial or username already exist!!")
+    }
+    //we're checking for avatar,
+    //Also, we've used .path so that we cannot know about
+    //the path where multer stored the file if in case anything goes wrong
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "avatar File is missing!!");
+    }
+    //let's upload the avatar 
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar){
+        throw new ApiError(400, "Avatar  is missing!!");
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar : avatar.url,
+        coverImage : coverImage?.url || "",
+        username: username.toLoweCase(),
+        email,
+        password
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(400 ,"Something went wrong while registering the user");
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200,"User is created Successfully", createdUser)
+    )
+
+
 })
 
-export {registerUser}
+ 
+export {
+    registerUser,
+}
